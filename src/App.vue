5698e26e7989c218c38dc4e8cf75aeb14e024ac8
anna <lyalinanna@gmail.com>
2019-09-08 22:15:35 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<a-locale-provider :locale="ru_RU">
+  <div id="app">
+    <Menu/>
+  </div>
+</a-locale-provider>
+</template>
+
+<script>
+import ru_RU from 'ant-design-vue/lib/locale-provider/ru_RU'
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      ru_RU,
+    }
+  },
+  components: {
+    Menu
+  }
+}
+</script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <a-menu v-model="currentTab" mode="horizontal" :theme="theme" @click="handleClick">
+      <a-menu-item key="user">
+        <a-icon type="user" />Пользователь
+      </a-menu-item>
+      <a-menu-item key="setting">
+        <a-icon type="setting" />Настройки
+      </a-menu-item>
+      <a-menu-item style="float: right">
+        <a-switch
+          defaultChecked
+          @change="changeTheme"
+          checkedChildren="Тёмная тема"
+          unCheckedChildren="Светлая тема"
+        />
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      currentTab: ["user"],
+      theme: "dark"
+    };
+  },
+  methods: {
+    handleClick(e) {
+      this.current = e.key;
+    },
+    changeTheme(checked) {
+      this.theme = checked ? "dark" : "light";
+    }
+  }
+};
+</script>
