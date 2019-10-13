68b2f1f96a8c9cf944a3821b2c1502315c44a358
anna <lyalinanna@gmail.com>
2019-10-13 23:39:15 +0300

    add new figures, but sth is wrong with calculation

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="home" @mousemove="followMouse">
         <div class="circle">
-            <div v-if="isVisible" class="triangle" :style="{ top: top, left: left}"></div>
+            <div v-if="isVisible" class="small-triangle" :style="{ top: top, left: left}"></div>
         </div>
         <div class="container">
             <div class="title">
                 <h1 class="header">Твоя домашняя страница</h1>
                 <strong class="subtitle">Расслабься и получай удовольствие</strong>
             </div>
-        </div>            
+        </div>
+        <div class="square">
+            <div v-if="isVisible" class="small-circle" :style="{ top: top, left: left}"></div>
+        </div>
+        <div class="triangle">
+            <div v-if="isVisible" class="small-square" :style="{ top: top, left: left}"></div>
+        </div>             
     </div>
 </template>
 
@@ -74,7 +80,36 @@ div#home{
     left: 80%;
     overflow: hidden;
 }
+#home .square {
+    position: absolute;
+    top: 320px;
+    right: 50%;
+    left: auto;
+    width: 600px;
+    height: 600px;
+    margin-right: 320px;
+    overflow: hidden;
+    background: #e79367;
+    margin-right: 120px;
+    transform: rotate(45deg) scale(.7);
+    cursor: none;
+    z-index: -1;
+}
 #home .triangle {
+    position: absolute;
+    z-index: -1;
+    top: 480px;
+    left: 40%;
+    width: 1000px;
+    height: 740px;
+    overflow: hidden;
+    margin-left: 60px;
+    border-color: transparent transparent #eac263;
+    border-style: solid;
+    border-width: 0 501px 740px;
+    transform: rotate(15deg) scale(.8);
+}
+#home .circle .small-triangle {
     position: absolute;
     width: 0;
     height: 0;
@@ -82,5 +117,20 @@ div#home{
     border-style: solid;
     border-width: 0 90px 150px;
     transform: translate(-50%,-50%) rotate(-20deg);
+}
+#home .triangle .small-square {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    background: #849d64;
+    transform: translate(-50%,-50%) rotate(15deg);
+}
+#home .square .small-circle {
+    position: absolute;
+    width: 220px;
+    height: 220px;
+    background: #221831;
+    border-radius: 100%;
+    transform: translate(-50%,-50%);
 }
 </style>
